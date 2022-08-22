5af3cddf5420cdfe158db8b37a54dfd7cca76756
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-08-22 17:47:52 +0530

    Time: 49 ms (26.70%), Space: 25 MB (39.03%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>ans;
+        int product = 1;
+        for(int i=0; i<nums.size(); i++){
+            product = product*nums[i];
+            ans.push_back(product);
+            // this will store left[] side multiplications
+        }
+        product =1;
+        for(int i=nums.size()-1; i>0; i-- ){
+            ans[i] = ans[i-1]*product;
+            product = product*nums[i];
+        }
+        ans[0] = product;
+        
+        return ans;
+    }
+};
