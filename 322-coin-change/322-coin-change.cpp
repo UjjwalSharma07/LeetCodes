b2f3daa32165fb016b920a2fd762a9177a2082ca
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-08-24 10:57:59 +0530

    Time: 84 ms (89.50%), Space: 14.5 MB (54.56%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,32 +1,65 @@
 class Solution {
 public:
-    int solve(vector<int>& coins,int target,vector<int>&dp){
+    // Memoazation Method
+    
+//     int solveMem(vector<int>& coins,int target,vector<int>&dp){
         
-        if(target == 0)
-            return 0;
+//         if(target == 0)
+//             return 0;
         
-        if(target<0)
-            return INT_MAX;
+//         if(target<0)
+//             return INT_MAX;
         
-        if(dp[target] != -1)
-            return dp[target];
+//         if(dp[target] != -1)
+//             return dp[target];
         
-        int mini = INT_MAX;
+//         int mini = INT_MAX;
         
-        for(int i=0; i<coins.size(); i++){
-            int ans = solve(coins,target-coins[i],dp);
+//         for(int i=0; i<coins.size(); i++){
+//             int ans = solveMem(coins,target-coins[i],dp);
             
-            if(ans != INT_MAX)
-                mini = min(mini,1+ans);
+//             if(ans != INT_MAX)
+//                 mini = min(mini,1+ans);
             
     
+//         }
+//         return dp[target] = mini;
+//     }
+    
+    // Tabulation Method
+    
+    int solveTab(vector<int>& coins,int t){
+         
+        vector<int>dp(t+1,INT_MAX);
+        dp[0]=0;
+         
+        for(int target=1; target<=t; target++){
+            
+            int mini = INT_MAX;
+            
+            for(int i=0; i<coins.size(); i++){
+                
+                if(target-coins[i] >=0){
+                    int ans = dp[target-coins[i]];
+                    
+                    if(ans != INT_MAX)
+                        mini = min(mini,1+ans);
+                }     
+            }
+            dp[target] = mini;
         }
-        return dp[target] = mini;
+        return dp[t];    
     }
+    
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int res= solve(coins,amount,dp);
+         // Memoazation Method
         
+        // vector<int>dp(amount+1,-1);
+        // int res= solveMem(coins,amount,dp);
+        
+        // Tabulation Method
+        
+        int res= solveTab(coins,amount);
         if(res == INT_MAX){
             return -1;
         }
