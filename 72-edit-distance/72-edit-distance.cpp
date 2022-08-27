81d03d937f4a1fe81495cf6b590780901f8826a4
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-08-27 10:24:35 +0530

    Time: 121 ms (15.25%), Space: 81.5 MB (7.05%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    // recursion
+    int solve(string a,string b,int i,int j){
+        if(i==a.length())
+            return b.length()-j;
+        if(j==b.length())
+            return a.length()-i;
+        
+        int ans =0;
+        if(a[i]==b[j]){
+            ans = solve(a,b,i+1,j+1);
+        }
+        else{
+            int insert = solve(a,b,i,j+1);
+            int del = solve(a,b,i+1,j);
+            int replace = solve(a,b,i+1,j+1);
+            
+            ans = min(insert,min(del,replace))+1;
+        }
+        return ans;
+    }
+    // memo
+     int solveMem(string a,string b,int i,int j,vector<vector<int>>& dp){
+         
+        if(i==a.length())
+            return b.length()-j;
+         
+        if(j==b.length())
+            return a.length()-i;
+         
+        if(dp[i][j] != -1)
+            return dp[i][j];
+            
+        int ans =0;
+        if(a[i]==b[j]){
+            ans = solveMem(a,b,i+1,j+1,dp);
+        }
+        else{
+            int insert = solveMem(a,b,i,j+1,dp);
+            int del = solveMem(a,b,i+1,j,dp);
+            int replace = solveMem(a,b,i+1,j+1,dp);
+            
+            ans = min(insert,min(del,replace))+1;
+        }
+        return dp[i][j] = ans;
+    }
+    
+    int minDistance(string word1, string word2) {
+        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,-1));
+        return solveMem(word1,word2,0,0,dp);
+        // return solve(word1,word2,0,0);
+    }
+};
