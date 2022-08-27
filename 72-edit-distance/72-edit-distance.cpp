7c7d1eced51d10cfb05dd134a16ae1c0f5efe1d9
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-08-27 10:41:15 +0530

    Time: 28 ms (38.25%), Space: 11.6 MB (16.86%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -21,7 +21,7 @@ public:
         return ans;
     }
     // memo
-     int solveMem(string a,string b,int i,int j,vector<vector<int>>& dp){
+    int solveMem(string a,string b,int i,int j,vector<vector<int>>& dp){
          
         if(i==a.length())
             return b.length()-j;
@@ -46,9 +46,43 @@ public:
         return dp[i][j] = ans;
     }
     
+    // tabulation
+    
+    int solveTab(string a,string b){
+        vector<vector<int>>dp(a.length()+1,vector<int>(b.length()+1,0));
+        
+        for(int j=0; j<b.length(); j++){
+            dp[a.length()][j] = b.length()-j;
+        }
+        for(int i=0; i<a.length(); i++){
+            dp[i][b.length()] = a.length()-i;
+        }
+        
+        for(int i=a.length()-1; i>=0; i--){
+            for(int j= b.length()-1; j>=0; j--){
+                int ans =0;
+                
+                if(a[i]==b[j]){
+                    ans = dp[i+1][j+1];
+                }
+                else{
+                    int insert = dp[i][j+1];
+                    int del = dp[i+1][j];
+                    int replace = dp[i+1][j+1];
+            
+                    ans = min(insert,min(del,replace))+1;
+                }
+                    dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
     int minDistance(string word1, string word2) {
         vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,-1));
-        return solveMem(word1,word2,0,0,dp);
+        
+        return solveTab(word1,word2);
+        // return solveMem(word1,word2,0,0,dp);
+        
         // return solve(word1,word2,0,0);
     }
 };
