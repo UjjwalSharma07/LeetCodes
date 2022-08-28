b233a78a8f5c23d692cc83f1af99c09711bbcb27
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-08-28 07:53:02 +0530

    Time: 3 ms (37.88%), Space: 6.1 MB (11.67%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -16,8 +16,24 @@ public:
         }
         return dp[n] = ans;
     }
+    int solveTab(int N){
+        vector<int>dp(N+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int n=2; n<=N; n++){
+            int ans = 0;
+        
+            for(int i=1; i<=n; i++){
+                ans+= dp[i-1]*dp[n-i];
+            }
+            dp[n] = ans;
+        }
+        return dp[N];
+    }
     int numTrees(int n) {
         vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        // return solve(n,dp);
+        return solveTab(n);
     }
 };
