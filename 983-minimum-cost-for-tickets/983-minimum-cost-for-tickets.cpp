c58babc145126626c836fdf69cfae7012c06aafe
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-08-30 20:15:51 +0530

    Time: 4 ms (76.47%), Space: 9.7 MB (35.68%) - LeetHub

---
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int solve(vector<int>& days, vector<int>& costs,int index){
+        if(index >= days.size()){
+            return 0;
+        }
+        
+        // for 1day pass
+        int option1 = costs[0]+solve(days,costs,index+1);
+        
+        // for 7days pass
+        int i;
+        // 7 th day k baad next index bhejne k liye 7 din aage bhda do
+        for(i=index; i<days.size() && days[i]<days[index]+7; i++); 
+        int option2 = costs[1]+solve(days,costs,i);
+        
+        // for 30days pass
+        
+        // 30 th day k baad next index bhejne k liye 30 din aage bhda do
+        for(i=index; i<days.size() && days[i]<days[index]+30; i++); 
+        int option3 = costs[2]+solve(days,costs,i);
+        
+        return min(option1,min(option2,option3));
+    }
+    
+    // memo
+    int solveMem(vector<int>& days, vector<int>& costs,int index, vector<int>& dp){
+        if(index >= days.size()){
+            return 0;
+        }
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        // for 1day pass
+        int option1 = costs[0]+solveMem(days,costs,index+1,dp);
+        
+        // for 7days pass
+        int i;
+        // 7 th day k baad next index bhejne k liye 7 din aage bhda do
+        for(i=index; i<days.size() && days[i]<days[index]+7; i++); 
+        int option2 = costs[1]+solveMem(days,costs,i,dp);
+        
+        // for 30days pass
+       
+        // 30 th day k baad next index bhejne k liye 30 din aage bhda do
+        for(i=index; i<days.size() && days[i]<days[index]+30; i++); 
+        int option3 = costs[2]+solveMem(days,costs,i,dp);
+        
+        return dp[index] = min(option1,min(option2,option3));
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int index= 0;
+        // solve(days,costs,index);
+        int n=days.size();
+        vector<int>dp(n+1,-1);
+        return solveMem(days,costs,index,dp);
+    }
+};
