51bd4b876687ab54d2104191b52d3223af51f404
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-11-12 13:04:39 +0530

    Added solution - LeetHub

---
+++ Maximum Sub-String after at most K changes - GFG/maximum-sub-string-after-at-most-k-changes.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		int characterReplacement(string s, int k){
+		    int i=0;
+		    int j=0;
+		    int ans=0;
+		    int maxi = INT_MIN;
+		    
+		    unordered_map<char,int>mp;
+		    
+		    while(j<s.size()){
+		        mp[s[j]]++;
+		        
+		        maxi = max(maxi,mp[s[j]]);
+		        
+		        int max_size = j-i+1-maxi;
+		        
+		        if(max_size>k){
+		            mp[s[i]]--;
+		            i++;
+		        }
+		        ans = max(ans,j-i+1);
+		        j++;
+		    }
+		    return ans;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		int k;
+		cin >> k;
+		Solution obj;
+		int ans = obj.characterReplacement(s, k);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
