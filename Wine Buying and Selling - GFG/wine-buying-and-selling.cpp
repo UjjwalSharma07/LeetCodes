2c3d6627b8ccc65ec3bd4c20e981e87f8901f47b
Ujjwal Sharma <94548686+UjjwalSharma07@users.noreply.github.com>
2022-12-24 10:50:04 +0530

    Added solution - LeetHub

---
+++ Wine Buying and Selling - GFG/wine-buying-and-selling.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+  public:
+  long long int wineSelling(vector<int>& Arr, int N){
+      long long int ans =0;
+      
+      for(int i=1;i<N;i++)
+      {
+        ans+=abs(Arr[i-1]);
+        Arr[i]=Arr[i]+Arr[i-1];
+      }
+      return ans;
+  }
+};
+
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        long long int ans = ob.wineSelling(arr,n);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
